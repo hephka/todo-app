72140db86b99fc6563b491c64037f833618f9aba
hephka <franckthevenin95@gmail.com>
2020-09-09 22:01:03 +0200

    tuto cours ok

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// src/index.js
+
+import React from "react"
+import ReactDOM from "react-dom"
+import "bootstrap/dist/css/bootstrap.css"
+import App from "./App"
+import { unregister } from './serviceWorker';
+// ... rien ne change ensuite
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+unregister();
